9070d5171b6432920b2f8c94ecfc5d02e6bdaa4e
MarianaMarques06 <marianascottmarques@gmail.com>
2026-02-17 17:05:19 +0000

    Add comments

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Contain the main program
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Print "Hello LP!" and "Bye world"
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP!");
