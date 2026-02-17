b916aa47a45566166a5f9f1b5b9d8852c596ff58
MarianaMarques06 <marianascottmarques@gmail.com>
2026-02-17 17:55:14 +0000

    Add real float, double, and decimal variables

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -20,6 +20,7 @@ namespace VariousTypes
             Console.WriteLine(j);
             Console.WriteLine(k);
             Console.WriteLine(n);
+
         //Char variables with different Unicode values
             char sword = '\u2694';
             char scissors = '\u2702';
@@ -29,6 +30,17 @@ namespace VariousTypes
             Console.WriteLine(sword);
             Console.WriteLine(scissors);
             Console.WriteLine(shamrock);
+
+        //Real variables of different types initialized
+            float x = 3.141516f;
+            double y = 2.71645;
+            decimal z = 1.6180m;
+
+        //Display the value on the screen
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
         }
     }
 }
