119ff3ece76164b4b3f750bb6794aa39d0a8f6fa
MarianaMarques06 <marianascottmarques@gmail.com>
2026-02-17 17:23:25 +0000

    Add integer variables of various types to the VariousTypes project

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+        //Integer variables of different types
+            int i = 8;
+            uint j = 10u;
+            long k = 12L;
+            ulong n = 45L;
+        //Display the value on the screen 
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+            Console.WriteLine(n);
+        }
+    }
+}
