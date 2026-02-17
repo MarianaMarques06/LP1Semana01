bf8f588fbf971d623855b105c641314562b0177d
MarianaMarques06 <marianascottmarques@gmail.com>
2026-02-17 17:46:50 +0000

    Add char variables with different Unicode values

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace VariousTypes
 {
@@ -6,16 +7,28 @@ namespace VariousTypes
     {
         private static void Main(string[] args)
         {
+        
+        Console.OutputEncoding = Encoding.UTF8;
+
         //Integer variables of different types
             int i = 8;
             uint j = 10u;
             long k = 12L;
-            ulong n = 45L;
+            ulong n = 45UL;
         //Display the value on the screen 
             Console.WriteLine(i);
             Console.WriteLine(j);
             Console.WriteLine(k);
             Console.WriteLine(n);
+        //Char variables with different Unicode values
+            char sword = '\u2694';
+            char scissors = '\u2702';
+            char shamrock = '\u2618';
+
+        //Display the value on the screen
+            Console.WriteLine(sword);
+            Console.WriteLine(scissors);
+            Console.WriteLine(shamrock);
         }
     }
 }
