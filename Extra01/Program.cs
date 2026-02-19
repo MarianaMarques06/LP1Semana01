c5ff87eb89631a7cedd4f1cd818af9ddfde63cc9
MarianaMarques06 <marianascottmarques@gmail.com>
2026-02-19 16:45:16 +0000

    Rename the project

---
+++ Extra01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace Extra01
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            char Trademark = '\u00AE';
+
+            Console.WriteLine(Trademark);
+        }
+    }
+}
